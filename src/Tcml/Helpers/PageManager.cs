445e034fa7a606776a332fa4031e04a40b7f417e
ArchLeaders <archleadership28@gmail.com>
2024-01-03 20:15:37 -0800

    Fix a dumb mistake

--- src/Tcml/Helpers/PageManager.cs
+++ src/Tcml/Helpers/PageManager.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAvalonia.UI.Controls;
+using System.Collections.ObjectModel;
 using Tcml.Helpers.Models;
 
 namespace Tcml.Helpers;
@@ -15,15 +16,28 @@ public class PageManager
     private static readonly Lazy<PageManager> _shared = new(() => new());
     public static PageManager Shared => _shared.Value;
 
-    public Dictionary<Page, PageModel> Pages { get; } = [];
+    private readonly Dictionary<Page, int> _lookup = [];
+    public ObservableCollection<PageModel> Pages { get; } = [];
 
     public void Register(Page page, string title, object? content, Symbol icon, string? description = null)
     {
-        Pages.Add(page, new PageModel {
+        _lookup[page] = Pages.Count;
+
+        Pages.Add(new PageModel {
             Title = title,
             Content = content,
             Description = description,
             Icon = icon
         });
+    }
+
+    public PageModel Get(Page page)
+    {
+        return Pages[_lookup[page]];
+    }
+
+    public T Get<T>(Page page) where T : PageModel
+    {
+        return (T)Pages[_lookup[page]];
     }
 }
