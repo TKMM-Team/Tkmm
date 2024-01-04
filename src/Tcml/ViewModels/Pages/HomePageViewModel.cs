e2f3dba4c3c3924262bd87a4910b4f78bbff2e4d
ArchLeaders <archleadership28@gmail.com>
2024-01-03 22:38:41 -0800

    Bind description text

--- src/Tcml/ViewModels/Pages/HomePageViewModel.cs
+++ src/Tcml/ViewModels/Pages/HomePageViewModel.cs
@@ -5,6 +5,9 @@ namespace Tcml.ViewModels.Pages;
 
 public partial class HomePageViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private string _description = string.Empty;
+
     [RelayCommand]
     public void SetStatus()
     {
