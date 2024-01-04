a0c14ef0dffd12fa2f4b19c9bb49c5990b4606c7
ArchLeaders <archleadership28@gmail.com>
2024-01-04 01:34:48 -0800

    Add StorageFolder config

--- src/Tcml.Core/Config.cs
+++ src/Tcml.Core/Config.cs
@@ -1,10 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ConfigFactory.Core.Attributes;
 using ConfigFactory.Core;
 
 namespace Tcml.Core;
 
 public partial class Config : ConfigModule<Config>
 {
+    private static readonly string _defaultPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "tkmm");
+
     public static Action<string>? SetTheme { get; set; }
 
     [ObservableProperty]
@@ -14,6 +17,17 @@ public partial class Config : ConfigModule<Config>
         Group = "Application")]
     [property: ConfigFactory.Core.Attributes.DropdownConfig("Dark", "Light")]
     private string _theme = "Dark";
+
+    [ObservableProperty]
+    [property: ConfigFactory.Core.Attributes.Config(
+        Header = "System Folder",
+        Description = "The folder used to store TKMM system files.",
+        Group = "Application")]
+    [property: ConfigFactory.Core.Attributes.BrowserConfig(
+        BrowserMode = BrowserMode.OpenFolder,
+        InstanceBrowserKey = "config-storage-folder",
+        Title = "Storage Folder")]
+    private string _storageFolder = _defaultPath;
 
     partial void OnThemeChanged(string value)
     {
