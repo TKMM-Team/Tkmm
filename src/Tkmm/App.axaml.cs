0800d58a10d8017be715c8d761833e650061676a
ArchLeaders <archleadership28@gmail.com>
2024-01-05 01:36:03 -0800

    Update about page icon

--- src/Tkmm/App.axaml.cs
+++ src/Tkmm/App.axaml.cs
@@ -59,7 +59,7 @@ public partial class App : Application
 
             PageManager.Shared.Register(Page.Home, "Home", new HomePageView(), Symbol.Home, "Home");
             PageManager.Shared.Register(Page.Tools, "Dev Tools", new ToolsPageView(), Symbol.CodeHTML, "Mod Developer Tools");
-            PageManager.Shared.Register(Page.About, "About", new AboutPageView(), Symbol.Help, "About The Project", isFooter: true);
+            PageManager.Shared.Register(Page.About, "About", new AboutPageView(), Symbol.Bookmark, "About The Project", isFooter: true);
 
             shellView.MainNavigation.SelectedItem = PageManager.Shared.Pages[0];
         }
