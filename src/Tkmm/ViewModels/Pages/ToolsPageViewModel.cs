f2d897e28747507b9b71294b20ddb1270983f605
ArchLeaders <archleadership28@gmail.com>
2024-01-22 22:00:21 -0800

    Add Import/Export metadata commands

--- src/Tkmm/ViewModels/Pages/ToolsPageViewModel.cs
+++ src/Tkmm/ViewModels/Pages/ToolsPageViewModel.cs
@@ -2,6 +2,8 @@
 using CommunityToolkit.Mvvm.Input;
 using ConfigFactory.Avalonia.Helpers;
 using ConfigFactory.Core.Attributes;
+using System.IO.Compression;
+using System.Text.Json;
 using Tkmm.Core.Components;
 using Tkmm.Core.Models.Mods;
 
@@ -9,6 +11,10 @@ namespace Tkmm.ViewModels.Pages;
 
 public partial class ToolsPageViewModel : ObservableObject
 {
+    private static readonly JsonSerializerOptions _jsonOptions = new() {
+        WriteIndented = true
+    };
+
     [ObservableProperty]
     private string _exportFile = string.Empty;
 
@@ -51,5 +57,45 @@ public partial class ToolsPageViewModel : ObservableObject
         packageGenerator.Save(ExportFile, clearOutputFolder: true);
 
         AppStatus.Set("Package built");
+    }
+
+    [RelayCommand]
+    private async Task ImportInfo()
+    {
+        BrowserDialog dialog = new(BrowserMode.OpenFile, "Import Mod Info", "JSON Metadata:info.json|Mod Archive:*.tkcl");
+        if (await dialog.ShowDialog() is string result) {
+            Stream? stream;
+            if (result.EndsWith("info.json")) {
+                stream = File.OpenRead(result);
+            }
+            else {
+                ZipArchive archive = ZipFile.OpenRead(result);
+                stream = archive.Entries.FirstOrDefault(x => x.Name == "info.json")?.Open();
+            }
+
+            if (stream is null) {
+                AppStatus.Set("Could not read mod metadata!", "fa-solid fa-triangle-exclamation", temporaryStatusTime: 1.5, isWorkingStatus: false);
+                return;
+            }
+
+            Mod = JsonSerializer.Deserialize<Mod>(stream)
+                ?? throw new InvalidOperationException("""
+                    Error parsing metadata: The JSON deserializer returned null.
+                    """);
+
+            await stream.DisposeAsync();
+        }
+    }
+
+    [RelayCommand]
+    private async Task ExportInfo()
+    {
+        BrowserDialog dialog = new(BrowserMode.SaveFile, "Export Mod Info", "JSON Metadata:info.json", "info.json");
+        if (await dialog.ShowDialog() is string result) {
+            using FileStream fs = File.Create(result);
+            JsonSerializer.Serialize(fs, Mod, _jsonOptions);
+
+            AppStatus.Set("Exported mod metadata!", "fa-solid fa-circle-check", temporaryStatusTime: 1.5, isWorkingStatus: false);
+        }
     }
 }
