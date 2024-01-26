a917409b3677fafe81db48817b2a3530dc28498e
ArchLeaders <archleadership28@gmail.com>
2024-01-26 14:46:12 -0800

    Add launcher project

---
+++ src/Tkmm.Launcher/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Tkmm.Launcher.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ src/Tkmm.Launcher/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Tkmm.Launcher.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
