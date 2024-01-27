d3f69e601832c19dfd5dcfee98987355b744871a
ArchLeaders <archleadership28@gmail.com>
2024-01-26 16:59:00 -0800

    Do what 5th couldn't

--- src/Tkmm.Launcher/ViewModels/ShellViewModel.cs
+++ src/Tkmm.Launcher/ViewModels/ShellViewModel.cs
@@ -1,8 +1,42 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Threading.Tasks;
+using Tkmm.Core.Components;
+using Tkmm.Core.Helpers;
 
 namespace Tkmm.Launcher.ViewModels;
 
-public class ShellViewModel : ObservableObject
+public partial class ShellViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private string _primaryText = "Install";
 
+    public ShellViewModel()
+    {
+        Init();
+    }
+
+    [RelayCommand]
+    private static async Task Update()
+    {
+        await AppManager.Update();
+        await ToolHelper.DownloadDependencies();
+    }
+
+    [RelayCommand]
+    private static void Exit()
+    {
+        Environment.Exit(0);
+    }
+
+    private async void Init()
+    {
+        if (await AppManager.HasUpdate()) {
+            PrimaryText = "Update";
+        }
+        else {
+            PrimaryText = "Launch";
+        }
+    }
 }
