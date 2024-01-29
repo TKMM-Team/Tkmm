85d0bb493426fe84e53f9a969f44b2691178231f
The5thTear <144561947+The5thTear@users.noreply.github.com>
2024-01-29 11:02:58 -0700

    add extra launcher image just for fun (Background-F.png)

--- src/Tkmm.Launcher/Views/ShellView.axaml.cs
+++ src/Tkmm.Launcher/Views/ShellView.axaml.cs
@@ -15,6 +15,7 @@ public partial class ShellView : AppWindow
         ("Background-C.jpg", "#58b98a"),
         ("Background-D.jpg", "#bb9b45"),
         ("Background-E.jpg", "#a21e16"),
+        ("Background-F.png", "#c71916"),
     ];
 
     private static readonly List<(Bitmap Image, IBrush Color)> _backgrounds = [];
