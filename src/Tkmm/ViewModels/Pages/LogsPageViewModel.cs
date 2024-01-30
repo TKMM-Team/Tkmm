b560972f5a28d54f501180bd808ad557b16c76d9
ArchLeaders <archleadership28@gmail.com>
2024-01-30 02:43:37 -0800

    Do what 5th couldn't

--- src/Tkmm/ViewModels/Pages/LogsPageViewModel.cs
+++ src/Tkmm/ViewModels/Pages/LogsPageViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using Avalonia;
-using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Input.Platform;
 using Avalonia.Threading;
@@ -7,6 +6,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using Tkmm.Core;
 using Tkmm.Core.Models;
 using Tkmm.Views.Pages;
 
@@ -57,15 +57,28 @@ public partial class LogsPageViewModel(LogsPageView view) : ObservableObject
         return this;
     }
 
-    private class LogsListener(LogsPageViewModel vm) : TraceListener
+    private class LogsListener : TraceListener
     {
         private const string NULL = "(null)";
-        private readonly LogsPageViewModel _vm = vm;
+        private readonly string _logsPath = Path.Combine(Config.DocumentsFolder, "log.txt");
+        private readonly LogsPageViewModel _vm;
+        private readonly StreamWriter _writer;
+
+        public LogsListener(LogsPageViewModel vm)
+        {
+            _vm = vm;
+
+            FileStream fs = File.Create(_logsPath);
+            _writer = new(fs) {
+                AutoFlush = true
+            };
+        }
 
         public override void Write(string? message)
         {
             _vm.Logs.Add(new(message ?? NULL));
             Dispatcher.UIThread.Invoke(_vm._view.Viewer.ScrollToEnd);
+            _writer.WriteLine(message);
         }
 
         public override void WriteLine(string? message)
