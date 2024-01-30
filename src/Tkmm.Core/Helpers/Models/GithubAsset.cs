02a0bfd6b77c3f9493e7c009ad03109150c4ff9b
ArchLeaders <archleadership28@gmail.com>
2024-01-30 01:26:18 -0800

    Update asset resolver

--- src/Tkmm.Core/Helpers/Models/GithubAsset.cs
+++ src/Tkmm.Core/Helpers/Models/GithubAsset.cs
@@ -4,14 +4,20 @@ namespace Tkmm.Core.Helpers.Models;
 
 public class GithubAsset
 {
+    private const string LOCALAPPDATA_VAR = "%localappdata%";
+
     public required string Owner { get; set; }
     public required string Repo { get; set; }
     public required string Asset { get; set; }
+    public required string FilePath { get; set; }
 
     public async Task Download()
     {
         byte[] data = await GitHubOperations.GetAsset(Owner, Repo, Asset);
-        string outputFile = Path.Combine(Config.Shared.StaticStorageFolder, Path.GetFileName(Asset));
+        string outputFile = Path.GetFullPath(
+            Asset.Replace(LOCALAPPDATA_VAR, Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData))
+        );
+
         File.WriteAllBytes(outputFile, data);
     }
 }
