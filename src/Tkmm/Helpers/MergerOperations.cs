ab1142701ac6cf48293aad287f8ffa950a36fdab
ArchLeaders <archleadership28@gmail.com>
2024-05-01 16:29:48 -0700

    Update set status

--- src/Tkmm/Helpers/MergerOperations.cs
+++ src/Tkmm/Helpers/MergerOperations.cs
@@ -11,8 +11,10 @@ public static class MergerOperations
             await MergerService.Merge();
         }
         catch (Exception ex) {
+            TriviaService.Stop();
             App.ToastError(ex);
             AppLog.Log(ex);
+            AppStatus.Set(ex.Message, isWorkingStatus: false, temporaryStatusTime: 1.5, logLevel: LogLevel.None);;
         }
     }
 }
