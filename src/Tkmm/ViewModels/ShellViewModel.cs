5aa643e55dc2af26d04a9571626f9556b6652421
ArchLeaders <archleadership28@gmail.com>
2024-06-11 12:36:13 -0700

    Code cleanup

--- src/Tkmm/ViewModels/ShellViewModel.cs
+++ src/Tkmm/ViewModels/ShellViewModel.cs
@@ -1,14 +1,7 @@
-﻿using Avalonia.Controls;
-using CommunityToolkit.Mvvm.ComponentModel;
-using Tkmm.Views.Pages;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Tkmm.ViewModels;
 
 public partial class ShellViewModel : ObservableObject
 {
-    [ObservableProperty]
-    private string _status = "Ready";
-
-    [ObservableProperty]
-    private UserControl _page = new HomePageView();
 }
