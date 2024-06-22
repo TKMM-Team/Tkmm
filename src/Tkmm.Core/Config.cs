b90157279f4255edd74ddbf5d33e93ca299e5702
ArchLeaders <archleadership28@gmail.com>
2024-06-22 10:20:36 -0700

    Whoops

--- src/Tkmm.Core/Config.cs
+++ src/Tkmm.Core/Config.cs
@@ -108,7 +108,7 @@ public partial class Config : ConfigModule<Config>
         SetTheme?.Invoke(value);
     }
 
-    private static readonly string _ryujinxPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Ryujinx", "mods", "contents", "0100f2c0115b6000");
+    private static readonly string _ryujinxPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Ryujinx", "mods", "contents", "0100f2c0115b6000", "TKMM");
     partial void OnUseRyujinxChanged(bool value)
     {
         if (Directory.Exists(_ryujinxPath)) {
