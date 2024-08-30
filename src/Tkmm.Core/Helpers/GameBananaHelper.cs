92bf2806d23523b1beef71cc8c7b49fcaff81240
Evan Husted <gr33m11@gmail.com>
2024-08-29 21:38:37 -0500

    attempt 1

--- src/Tkmm.Core/Helpers/GameBananaHelper.cs
+++ src/Tkmm.Core/Helpers/GameBananaHelper.cs
@@ -4,17 +4,19 @@ namespace Tkmm.Core.Helpers;
 
 public static class GameBananaHelper
 {
-    public static bool IsOnline {
-        get {
+    private static readonly Lazy<bool> _isOnline = new(() =>
+        {
             try {
                 return new Ping()
-                    .Send("www.google.com", 1000).Status == IPStatus.Success;
+                    .Send("gamebanana.com", 10000).Status == IPStatus.Success;
             }
             catch {
                 return false;
             }
-        }
-    }
+        });
+
+
+    public static bool IsOnline => _isOnline.Value;
 
     private const string BASE_URL = "https://gamebanana.com/apiv11";
     private static readonly HttpClient _client = new();
