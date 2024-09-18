95f43a14ca0564eeda59cb9a1460328880e9936c
ArchLeaders <archleadership28@gmail.com>
2024-09-18 10:59:07 -0700

    Initial cleanup

--- src/Tkmm.Core/Models/GameBanana/GameBananaMedia.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Tkmm.Core.Models.GameBanana;
-
-public class GameBananaMedia
-{
-    [JsonPropertyName("_aImages")]
-    public List<GameBananaImage> Images { get; set; } = [];
-}
