33b04425735db77dd684d3e0a665b8a647000db3
ArchLeaders <archleadership28@gmail.com>
2024-09-20 14:36:18 -0700

    Create Extracted IRomfs implementation

---
+++ src/Clients/Tkmm.Desktop/IO/ExtractedRomfs.cs
@@ -0,0 +1,14 @@
+using Revrs.Buffers;
+using Tkmm.Core.Abstractions.IO;
+using TotkCommon;
+
+namespace Tkmm.Desktop.IO;
+
+public sealed class ExtractedRomfs(ITkFileSystem fs) : IRomfs
+{
+    public ArraySegmentOwner<byte> GetVanilla(string fileName, out int zsDictionaryId)
+    {
+        string absoluteFilePath = Path.Combine(Totk.Config.GamePath, fileName);
+        return fs.OpenReadAndDecompress(absoluteFilePath, out zsDictionaryId);
+    }
+}
