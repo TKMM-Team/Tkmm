17472cef7fa3dd8976a42d1c987fd084b99a9f62
ArchLeaders <archleadership28@gmail.com>
2024-09-20 13:52:04 -0700

    Cleanup desktop project

--- src/Clients/Tkmm.Desktop/Program.cs
+++ src/Clients/Tkmm.Desktop/Program.cs
@@ -1,86 +1,41 @@
 ﻿using Avalonia;
-using Avalonia.Threading;
-using Cocona;
+using Microsoft.Extensions.Logging;
+using Ninject;
 using Projektanker.Icons.Avalonia;
 using Projektanker.Icons.Avalonia.FontAwesome;
 using Tkmm.Components;
-using Tkmm.Core.Commands;
-using Tkmm.Core.Components;
-using Tkmm.Helpers;
+using Tkmm.Core;
 
 namespace Tkmm.Desktop;
 
 internal abstract class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
     [STAThread]
     public static void Main(string[] args)
     {
-        if (AppManager.Start(args, Attach) == false) {
-            return;
-        }
+        TKMM.DI.Load<DesktopModule>();
 
-        if (CheckArgs(args) == 0) {
-            try {
-                BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
-            }
-            catch
-#if !DEBUG
-            (Exception ex)
-#endif
-            {
+        try {
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception ex) {
+            TKMM.Logger.LogError(ex, "An unhandled exception of type '{ErrorType}' occured.", ex.GetType());
 #if DEBUG
-                throw;
-#else
-                Tkmm.Core.Helpers.Operations.ConsoleOperations.WaitOnFailure(ex);
+            throw;
 #endif
-            }
-
-            return;
         }
-
-        CoconaApp app = CoconaApp.Create();
-        app.AddCommands<GeneralCommands>();
-        app.Run();
     }
 
-    public static async Task Attach(string[] args)
-    {
-        if (CheckArgs(args) == 0) {
-            Dispatcher.UIThread.Invoke(App.Focus);
-            return;
-        }
-
-        CoconaApp app = CoconaApp.Create(args);
-        app.AddCommands<GeneralCommands>();
-        await app.RunAsync();
-    }
-
-    private static int CheckArgs(string[] args)
-    {
-        int argc = args.Length;
-        foreach (string arg in args.Where(x => Path.Exists(x) || (x.Length > 5 && x.AsSpan()[..5] is "tkmm:"))) {
-            argc--;
-            _ = Task.Run(async () => {
-                await ModHelper.Import(arg);
-            });
-        }
-
-        return argc;
-    }
-
-    // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
     {
         IconProvider.Current
             .Register(new FontAwesomeIconProvider(FontAwesomeJsonStreamProvider.Instance));
 
 #if DEBUG
-        if (OperatingSystem.IsWindows() && Core.Config.Shared.ShowConsole == false) {
-            Core.Helpers.Windows.WindowsOperations.SetWindowMode(Core.Helpers.Windows.WindowMode.Hidden);
-        }
+        // if (OperatingSystem.IsWindows() && Config.Shared.ShowConsole == false) {
+        //     Core.Helpers.Windows.WindowsOperations.SetWindowMode(Core.Helpers.Windows.WindowMode.Hidden);
+        // }
 #endif
 
         return AppBuilder.Configure<App>()
