8457ba6075353c756a9e79c962629f5a0bae33d6
ArchLeaders <archleadership28@gmail.com>
2024-09-21 13:16:56 -0700

    Support pre-parse check string parsing

--- src/Tkmm.Core.Abstractions/Parsers/ITkModParser.cs
+++ src/Tkmm.Core.Abstractions/Parsers/ITkModParser.cs
@@ -2,5 +2,9 @@ namespace Tkmm.Core.Abstractions.Parsers;
 
 public interface ITkModParser
 {
-    ValueTask<ITkMod> Parse(in Stream metadataStream);
+    ValueTask<bool> CanParseInput(string input, CancellationToken ct = default);
+    
+    ValueTask<ITkMod> Parse(string input, CancellationToken ct = default);
+
+    ValueTask<ITkMod> Parse(Stream input, CancellationToken ct = default);
 }
