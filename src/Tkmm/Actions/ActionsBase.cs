dbb8b256eff5b1d3d8251c1a1eb1a97ea896e8d4
ArchLeaders <archleadership28@gmail.com>
2024-10-06 18:58:48 -0700

    Impl ActionsBase class

---
+++ src/Tkmm/Actions/ActionsBase.cs
@@ -0,0 +1,88 @@
+using System.Runtime.CompilerServices;
+using Avalonia.Controls;
+using Avalonia.Media;
+using FluentAvalonia.UI.Controls;
+using Microsoft.Extensions.Logging;
+using Tkmm.Core;
+
+namespace Tkmm.Actions;
+
+public abstract class ActionsBase<TSingleton> where TSingleton : ActionsBase<TSingleton>, new()
+{
+    protected abstract string ActionGroupName { get; }
+
+    public static TSingleton Instance { get; } = new();
+
+    /// <summary>
+    /// Run pre-action checks to ensure the target action can run.
+    /// </summary>
+    protected async ValueTask<bool> CanActionRun([CallerMemberName] string? actionName = null, bool showErrorDialog = true)
+    {
+        TKMM.Logger.LogInformation(
+            "[TKMM] [{UtcNow}] Validatiing {ActionName} from {ActionGroupName}.",
+            DateTime.UtcNow, actionName, ActionGroupName);
+        
+        LogInfo();
+
+        if (EnsureConfiguration()) {
+            TKMM.Logger.LogInformation(
+                "[TKMM] [{UtcNow}] Executing {ActionName} from {ActionGroupName}.",
+                DateTime.UtcNow, actionName, ActionGroupName);
+            
+            return true;
+        }
+
+        TKMM.Logger.LogCritical(
+            "[TKMM] [{UtcNow}] Failed to run {ActionName} from {ActionGroupName}, the configuration was invalid.",
+            DateTime.UtcNow, actionName, ActionGroupName);
+
+        if (showErrorDialog) {
+            await ShowFailureDialog(actionName,
+                "The application configuration is invalid, the invoked action cannot run.");
+        }
+        
+        return false;
+    }
+
+    /// <summary>
+    /// Show a dialog with an error message.
+    /// </summary>
+    protected async ValueTask ShowFailureDialog(string? actionName, string errorMessage)
+    {
+        ContentDialog dialog = new() {
+            Title = $"Failed to run {actionName}",
+            Content = new TextBlock {
+                Text = errorMessage,
+                TextWrapping = TextWrapping.WrapWithOverflow
+            },
+            DefaultButton = ContentDialogButton.Primary,
+            PrimaryButtonText = "OK"
+        };
+
+        await dialog.ShowAsync();
+    }
+
+    /// <summary>
+    /// Log information about the configured application for end-user debugging.
+    /// </summary>
+    public static void LogInfo()
+    {
+        TKMM.Romfs.LogInfo(TKMM.Logger);
+        
+        TKMM.Logger.LogInformation(
+            "[TKMM] [{UtcNow}] Game Language: {GameLanguage}.", DateTime.UtcNow, TKMM.Config.GameLanguage);
+    }
+
+    /// <summary>
+    /// Ensure the application is configured correctly.
+    /// </summary>
+    protected virtual bool EnsureConfiguration()
+    {
+        if (!TKMM.Romfs.IsStateValid(out string? invalidReason)) {
+            TKMM.Logger.LogCritical("Invalid RomFS: {Reason}.", invalidReason);
+            return false;
+        }
+
+        return true;
+    }
+}
