1acbf13fae0b00e95c5da40d4274368e043bb64f
ArchLeaders <archleadership28@gmail.com>
2024-10-06 21:39:25 -0700

    Update app (init core)

--- src/Tkmm/App.axaml.cs
+++ src/Tkmm/App.axaml.cs
@@ -5,6 +5,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Controls.Notifications;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
 using Avalonia.Styling;
 using Avalonia.Threading;
 using ConfigFactory;
@@ -16,18 +17,21 @@ using ConfigFactory.Models;
 using FluentAvalonia.UI.Controls;
 using MenuFactory;
 using MenuFactory.Abstractions;
+using Microsoft.Extensions.Logging;
+using Tkmm.Actions;
 using Tkmm.Builders;
 using Tkmm.Builders.MenuModels;
+using Tkmm.Components;
 using Tkmm.Core;
-using Tkmm.Core.Components;
-using Tkmm.Core.Helpers;
-using Tkmm.Core.Helpers.Windows;
+using Tkmm.Core.Abstractions.Common;
+using Tkmm.Dialogs;
+using Tkmm.Extensions;
 using Tkmm.Helpers;
 using Tkmm.ViewModels;
 using Tkmm.Views;
 using Tkmm.Views.Pages;
 using TotkCommon;
-using WindowsOperations = Tkmm.Core.Helpers.Windows.WindowsOperations;
+using PageManager = Tkmm.Components.PageManager;
 
 namespace Tkmm;
 
@@ -37,16 +41,15 @@ public class App : Application
 
     public static readonly string Version = typeof(App).Assembly
         .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
-        .InformationalVersion.Split('+')[0] ?? "Undefined Version";
-    public static string Title { get; } = $"TotK Mod Manager";
-    public static string ShortTitle { get; } = $"TKMM v{Version}";
-    public static string ReleaseUrl { get; } = $"https://github.com/TKMM-Team/Tkmm/releases/{Version}";
-    public static TopLevel? XamlRoot { get; private set; }
+        .InformationalVersion.Split('+')[0] ?? SystemMsg.UndefinedVersion;
 
-    /// <summary>
-    /// Application <see cref="IMenuFactory"/> (used for extending the main menu at runtime)
-    /// </summary>
+    public static string Title { get; } = $"TotK Mod Manager";
+
+    public static string ShortTitle { get; } = $"TKMM v{Version}";
+
     public static IMenuFactory MenuFactory { get; private set; } = null!;
+    
+    public static TopLevel XamlRoot { get; private set; } = null!;
 
     static App()
     {
@@ -55,8 +58,17 @@ public class App : Application
 
     public App()
     {
-        TaskScheduler.UnobservedTaskException += (_, e) => {
-            ToastError(e.Exception);
+        TKMM.Logger.LogInformation(
+            "[TKMM] [{UtcNow}] Version: {Version}", DateTime.UtcNow, Version);
+        
+        TaskScheduler.UnobservedTaskException += async (_, e) => {
+            object errorReportResult = await ErrorDialog.ShowAsync(e.Exception, TaskDialogButton.OKButton, TaskDialogButton.RetryButton);
+            if (Equals(errorReportResult, TaskDialogButton.RetryButton)) {
+                e.SetObserved();
+                return;
+            }
+
+            await SystemActions.Instance.SoftClose();
         };
     }
 
@@ -67,44 +79,37 @@ public class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        // Line below is needed to remove Avalonia data validation.
-        // Without this line you will get duplicate validations from both Avalonia and CT
         BindingPlugins.DataValidators.RemoveAt(0);
-
-        if (OperatingSystem.IsWindows()) {
-            WindowsOperations.SetWindowMode(Config.Shared.ShowConsole ? WindowMode.Visible : WindowMode.Hidden);
-        }
+        IThumbnail.CreateBitmap = stream => new Bitmap(stream);
 
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
             ShellView shellView = new() {
                 DataContext = new ShellViewModel()
             };
 
-            shellView.Closed += (_, _) => {
-                ProfileManager.Shared.Apply();
-                Config.Shared.Save();
+            shellView.Closed += async (_, _) => {
+                await SystemActions.Instance.SoftClose();
             };
 
             XamlRoot = shellView;
             shellView.Loaded += (_, _) => {
-                _notificationManager = new(XamlRoot) {
+                _notificationManager = new WindowNotificationManager(XamlRoot) {
                     Position = NotificationPosition.BottomRight,
                     MaxItems = 5,
-                    Margin = new(0, 0, 4, 30)
+                    Margin = new Thickness(0, 0, 4, 30)
                 };
             };
 
             MenuFactory = new AvaloniaMenuFactory(XamlRoot);
-            MenuFactory.AddMenuGroup<ShellViewMenu>();
+            MenuFactory.ConfigureMenu();
 
             shellView.MainMenu.ItemsSource = MenuFactory.Items;
             desktop.MainWindow = shellView;
 
             // ConfigFactory Configuration
-            BrowserDialog.StorageProvider = desktop.MainWindow.StorageProvider;
-            Config.SetTheme = (theme) => {
-                RequestedThemeVariant = theme == "Dark" ? ThemeVariant.Dark : ThemeVariant.Light;
-            };
+            BrowserDialog.StorageProvider = shellView.StorageProvider;
+
+            Config.Shared.ThemeChanged += OnThemeChanged;
 
             ConfigPage settingsPage = new();
             bool isValid = false;
@@ -115,94 +120,63 @@ public class App : Application
                 isValid = ConfigModule<Config>.Shared.Validate(out string? _, out ConfigProperty? target);
                 settingsModel.Append<Config>();
 
-                isValid = isValid && ConfigModule<TotkConfig>.Shared.Validate(out string? _, out target);
-                settingsModel.Append<TotkConfig>();
-
                 if (!isValid && target?.Attribute is not null) {
                     settingsModel.SelectedGroup = settingsModel.Categories
                         .Where(x => x.Header == target.Attribute.Category)
                         .SelectMany(x => x.Groups)
                         .FirstOrDefault(x => x.Header == target.Attribute.Group);
 
-                    AppStatus.Set($"Invalid setting, {target.Property.Name} is invalid.",
-                        "fa-solid fa-triangle-exclamation", isWorkingStatus: false);
+                    TkStatus.Set(Exceptions.InvalidSettings(target.Property.Name), TkIcons.WARNING);
                 }
             }
 
-            PageManager.Shared.Register(Page.Home, "Home", new HomePageView(), Symbol.Home, "Home", isDefault: true);
-            PageManager.Shared.Register(Page.Profiles, "Profiles", new ProfilesPageView(), Symbol.OtherUser, "Manage mod profiles");
-            PageManager.Shared.Register(Page.Tools, "TKCL Packager", new PackagingPageView(), Symbol.CodeHTML, "Mod developer tools");
-            PageManager.Shared.Register(Page.ShopParam, "ShopParam Overflow Editor", new ShopParamPageView(), Symbol.Sort, "ShopParam overflow ordering tools");
-            PageManager.Shared.Register(Page.Mods, "GameBanana Mod Browser", new GameBananaPageView(), Symbol.Globe, "GameBanana browser client for TotK mods");
+            PageManager.Shared.Register(Page.Home, PageMsg.Home, new HomePageView(), Symbol.Home, PageMsg.HomeDescription, isDefault: true);
+            PageManager.Shared.Register(Page.Profiles, PageMsg.Profiles, new ProfilesPageView(), Symbol.OtherUser, PageMsg.ProfilesDescription);
+            PageManager.Shared.Register(Page.Tools, PageMsg.Tools, new PackagingPageView(), Symbol.CodeHTML, PageMsg.ToolsDescription);
+            PageManager.Shared.Register(Page.ShopParam, PageMsg.ShopParam, new ShopParamPageView(), Symbol.Sort, PageMsg.ShopParamDescription);
+            PageManager.Shared.Register(Page.GbMods, PageMsg.GbMods, new GameBananaPageView(), Symbol.Globe, PageMsg.GbModsDescription);
 
-            PageManager.Shared.Register(Page.Logs, "Logs", new LogsPageView(), Symbol.AllApps, "System Logs", isFooter: true);
-            PageManager.Shared.Register(Page.Settings, "Settings", settingsPage, Symbol.Settings, "Settings", isFooter: true, isDefault: isValid == false);
+            PageManager.Shared.Register(Page.Logs, PageMsg.Logs, new LogsPageView(), Symbol.AllApps, PageMsg.LogsDescription, isFooter: true);
+            PageManager.Shared.Register(Page.Settings, PageMsg.Settings, settingsPage, Symbol.Settings, PageMsg.SettingsDescription, isFooter: true, isDefault: isValid == false);
 
-            Config.SetTheme(Config.Shared.Theme);
+            OnThemeChanged(Config.Shared.Theme);
         }
 
         base.OnFrameworkInitializationCompleted();
     }
+    
+    private void OnThemeChanged(string theme)
+    {
+        RequestedThemeVariant = theme switch {
+            "Dark" => ThemeVariant.Dark,
+            _ => ThemeVariant.Light
+        };
+    }
 
     public static void Focus()
     {
-        if (Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop && desktop.MainWindow is not null) {
-            desktop.MainWindow.WindowState = WindowState.Normal;
-            desktop.MainWindow.Activate();
+        if (Current?.ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime desktop || desktop.MainWindow is null) {
+            return;
         }
+
+        desktop.MainWindow.WindowState = WindowState.Normal;
+        desktop.MainWindow.Activate();
     }
 
-    public static void Toast(string message, string title = "Notice", NotificationType type = NotificationType.Information, TimeSpan? expiration = null, Action? action = null)
+    public static void Toast(string message, string? title = default, NotificationType type = NotificationType.Information,
+        TimeSpan? expiration = null, Action? action = null)
     {
         Dispatcher.UIThread.Invoke(() => {
             _notificationManager?.Show(
-                new Notification(title, message, type, expiration, action));
+                new Notification(title ??= SystemMsg.Notice, message, type, expiration, action));
         });
     }
 
     public static void ToastError(Exception ex)
     {
-        AppLog.Log(ex);
-
         Dispatcher.UIThread.Invoke(() => {
             _notificationManager?.Show(new Notification(
-                ex.GetType().Name, ex.Message, NotificationType.Error, onClick: () => {
-                    PageManager.Shared.Focus(Page.Logs);
-                }));
+                ex.GetType().Name, ex.Message, NotificationType.Error, onClick: () => { PageManager.Shared.Focus(Page.Logs); }));
         });
-    }
-
-    public static void LogTkmmInfo()
-    {
-        AppLog.Log($"App Version: '{Version}'", LogLevel.Info);
-
-        AppLog.Log($"Configured GamePath: '{TotkConfig.Shared.GamePath}'", LogLevel.Info);
-        AppLog.Log($"ZsDic Exists: '{File.Exists(TotkConfig.Shared.ZsDicPath)}'", LogLevel.Info);
-
-        AppLog.Log($"TotkCommon Configured GamePath: '{Totk.Config.GamePath}'", LogLevel.Info);
-        AppLog.Log($"TotkCommon ZsDic Exists: '{File.Exists(Totk.Config.ZsDicPath)}'", LogLevel.Info);
-    }
-
-    public static async Task PromptUpdate()
-    {
-        ContentDialog dialog = new() {
-            Title = "Update",
-            Content = """
-                An update is available.
-                
-                Would you like to close your current session and open the launcher?
-                """,
-            PrimaryButtonText = "Yes",
-            SecondaryButtonText = "Cancel"
-        };
-
-        if (await dialog.ShowAsync() == ContentDialogResult.Primary) {
-            await Task.Run(async () => {
-                await AppManager.UpdateLauncher();
-                AppManager.StartLauncher();
-            });
-
-            Environment.Exit(0);
-        }
     }
 }
