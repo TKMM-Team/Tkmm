700ca5ff85c3ad50c1478ee065b9cf50e9779785
ArchLeaders <archleadership28@gmail.com>
2024-10-07 11:14:39 -0700

    Create DirectoryHelper

---
+++ src/Tkmm/Helpers/DirectoryHelper.cs
@@ -0,0 +1,37 @@
+namespace Tkmm.Helpers;
+
+public static class DirectoryHelper
+{
+    public static void DeleteTargetsFromDirectory(string targetDirectory, string[] targets, bool recursive = false)
+    {
+        foreach (string target in targets) {
+            string absolutePath = Path.Combine(targetDirectory, target);
+            DeleteTarget(absolutePath, recursive);
+        }
+    }
+    
+    private static void DeleteTarget(string target, bool recursive = false)
+    {
+        if (File.Exists(target)) {
+            File.Delete(target);
+            return;
+        }
+        
+        if (Directory.Exists(target)) {
+            Directory.Delete(target, recursive);
+        }
+    }
+
+    public static void Copy(string source, string output, bool overwrite = false)
+    {
+        foreach (string sourceFile in Directory.EnumerateFiles(source)) {
+            string outputFile = Path.Combine(output, Path.GetFileName(sourceFile));
+            File.Copy(sourceFile, outputFile, overwrite);
+        }
+
+        foreach (string directory in Directory.EnumerateDirectories(source)) {
+            string outputDirectory = Path.Combine(output, Path.GetFileName(directory));
+            Copy(directory, outputDirectory, overwrite);
+        }
+    }
+}
