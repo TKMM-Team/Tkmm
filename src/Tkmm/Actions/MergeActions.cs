f55deaaadbf5158194dbc1a411029df346aedf7b
ArchLeaders <archleadership28@gmail.com>
2024-10-07 11:16:43 -0700

    Impl ExportToSdCard action

--- src/Tkmm/Actions/MergeActions.cs
+++ src/Tkmm/Actions/MergeActions.cs
@@ -11,22 +11,31 @@ using Tkmm.Builders;
 using Tkmm.Core;
 using Tkmm.Core.Abstractions;
 using Tkmm.Dialogs;
+using Tkmm.Helpers;
+using Tkmm.Models;
+using TotkCommon;
 
 namespace Tkmm.Actions;
 
 public sealed partial class MergeActions : GuardedActionGroup<MergeActions>
 {
     protected override string ActionGroupName { get; } = nameof(MergeActions).Humanize();
-    
+
     [RelayCommand]
-    public async Task Merge(CancellationToken ct = default)
+    public Task Merge(CancellationToken ct = default)
+    {
+        return Merge(TKMM.ModManager.CurrentProfile, ct);
+    }
+
+    public async Task Merge(ITkProfile profile, CancellationToken ct = default)
     {
         if (!await CanActionRun()) {
             return;
         }
-        
+
         // TODO: Find a better way to store conditional popups
-        if (!Config.Shared.ExportLocations.Any(x => x.IsEnabled) && !Config.Shared.SuppressExportLocationsPrompt) {
+        const string suppressMergeExportLocationsPrompt = "SuppressMergeExportLocationsPrompt";
+        if (!Config.Shared.ExportLocations.Any(exportLocation => exportLocation.IsEnabled) && !NamedDialogConfig.Shared[suppressMergeExportLocationsPrompt].IsSuppressed) {
             ContentDialog dialog = new() {
                 Title = "Warning",
                 Content = new StackPanel {
@@ -38,8 +47,8 @@ public sealed partial class MergeActions : GuardedActionGroup<MergeActions>
                         },
                         new CheckBox {
                             Content = "Never show again (recomended for Switch users).",
-                            DataContext = Config.Shared,
-                            [!ToggleButton.IsCheckedProperty] = new Binding(nameof(Config.SuppressExportLocationsPrompt))
+                            DataContext = NamedDialogConfig.Shared,
+                            [!ToggleButton.IsCheckedProperty] = NamedDialogConfig.GetBinding(suppressMergeExportLocationsPrompt)
                         }
                     }
                 },
@@ -53,35 +62,91 @@ public sealed partial class MergeActions : GuardedActionGroup<MergeActions>
                 if (await dialog.ShowAsync() is ContentDialogResult.Primary) {
                     await ExportLocationControlBuilder.Edit(Config.Shared.ExportLocations);
                 }
+
                 Config.Shared.Save();
             }
         }
 
         try {
             // TODO: Start the dumb trivia thing (if enabled)
-            await TKMM.ModManager.Merge(ct);
-            App.Toast($"The profile '{TKMM.ModManager.CurrentProfile.Name}' was merged successfully.",
+            await TKMM.ModManager.Merge(profile, ct);
+            App.Toast($"The profile '{profile.Name}' was merged successfully.",
                 "Merge Successful!", NotificationType.Success, TimeSpan.FromDays(5));
         }
         catch (Exception ex) {
             TKMM.Logger.LogError(ex, "An error occured when merging the selected profile '{Profile}'.",
-                TKMM.ModManager.CurrentProfile.Name);
+                profile.Name);
             await ErrorDialog.ShowAsync(ex);
         }
-    } 
-    
+    }
+
     [RelayCommand]
-    public async Task MergeProfile(ITkProfile profile, CancellationToken ct = default)
+    public Task ExportToSdCard(CancellationToken ct = default)
+    {
+        return ExportToSdCard(TKMM.ModManager.CurrentProfile, ct);
+    }
+
+    public async Task ExportToSdCard(ITkProfile profile, CancellationToken ct = default)
     {
         if (!await CanActionRun()) {
             return;
         }
 
+        DisplayDisk[] disks = DriveInfo.GetDrives()
+            .Where(static driveInfo => {
+                try {
+                    return driveInfo is {
+                        DriveType: DriveType.Removable,
+                        DriveFormat: "FAT32"
+                    };
+                }
+                catch {
+                    return false;
+                }
+            })
+            .Select(static driveInfo => new DisplayDisk(driveInfo))
+            .ToArray();
+
+        if (disks.Length is 0) {
+            await ErrorDialog.ShowAsync(
+                new DriveNotFoundException("No suitable disks found. Please make sure you have an SD card inserted or connected virtually over USB.")
+            );
+
+            return;
+        }
+
+        ContentDialog dialog = new() {
+            Title = "Select an SD card",
+            Content = new ComboBox {
+                ItemsSource = disks,
+                SelectedIndex = 0,
+                DisplayMemberBinding = new Binding("DisplayName")
+            },
+            PrimaryButtonText = "Merge and export",
+            SecondaryButtonText = "Cancel"
+        };
+
+        if (await dialog.ShowAsync() is not ContentDialogResult.Primary || dialog.Content is not ComboBox {
+                SelectedItem: DisplayDisk {
+                    Drive: DriveInfo drive
+                }
+            }) return;
+
+        await Merge(profile, ct);
+
         try {
-            await TKMM.ModManager.Merge(profile, ct);
+            string output = Path.Combine(drive.Name, "atmosphere", "contents", Totk.TITLE_ID);
+            
+            // TODO: Ask before deleting
+            // TODO: Abstract romfs, exefs folder names to constants
+            DirectoryHelper.DeleteTargetsFromDirectory(output, ["romfs", "exefs"], recursive: true);
+            
+            // TODO: Who decides the merge output location? (Should be an interface because it changes on TKMM-OS)
+            DirectoryHelper.Copy(".merge", output, overwrite: true);
         }
         catch (Exception ex) {
-            TKMM.Logger.LogError(ex, "An error occured when merging the profile '{Profile}'.", profile.Name);
+            TKMM.Logger.LogError(ex, "An error occured when exporting the profile '{Profile}' to the external drive " +
+                                     "'{TargetDrive}'.", profile.Name, drive.Name);
             await ErrorDialog.ShowAsync(ex);
         }
     }
