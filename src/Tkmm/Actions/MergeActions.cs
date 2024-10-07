92f12b9eb89715cfd7a30e0175507421c95fc9e8
ArchLeaders <archleadership28@gmail.com>
2024-10-06 22:21:17 -0700

    Stub merger operations helper

--- src/Tkmm/Actions/MergeActions.cs
+++ src/Tkmm/Actions/MergeActions.cs
@@ -1,6 +1,13 @@
+using Avalonia.Controls;
+using Avalonia.Controls.Notifications;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+using Avalonia.Media;
 using CommunityToolkit.Mvvm.Input;
+using FluentAvalonia.UI.Controls;
 using Humanizer;
 using Microsoft.Extensions.Logging;
+using Tkmm.Builders;
 using Tkmm.Core;
 using Tkmm.Core.Abstractions;
 using Tkmm.Dialogs;
@@ -17,12 +24,48 @@ public sealed partial class MergeActions : ActionsBase<MergeActions>
         if (!await CanActionRun()) {
             return;
         }
+        
+        // TODO: Find a better way to store conditional popups
+        if (!Config.Shared.ExportLocations.Any(x => x.IsEnabled) && !Config.Shared.SuppressExportLocationsPrompt) {
+            ContentDialog dialog = new() {
+                Title = "Warning",
+                Content = new StackPanel {
+                    Spacing = 5,
+                    Children = {
+                        new TextBlock {
+                            Text = "There are currently no export locations enabled. Would you like to configure them now?",
+                            TextWrapping = TextWrapping.WrapWithOverflow
+                        },
+                        new CheckBox {
+                            Content = "Never show again (recomended for Switch users).",
+                            DataContext = Config.Shared,
+                            [!ToggleButton.IsCheckedProperty] = new Binding(nameof(Config.SuppressExportLocationsPrompt))
+                        }
+                    }
+                },
+                PrimaryButtonText = "Yes",
+                SecondaryButtonText = "No",
+                DefaultButton = ContentDialogButton.Primary
+            };
+
+            // TODO: Abstract to another location (EditExportLocations)
+            {
+                if (await dialog.ShowAsync() is ContentDialogResult.Primary) {
+                    await ExportLocationControlBuilder.Edit(Config.Shared.ExportLocations);
+                }
+                Config.Shared.Save();
+            }
+        }
 
         try {
+            // TODO: Start the dumb trivia thing (if enabled)
             await TKMM.ModManager.Merge(ct);
+            App.Toast($"The profile '{TKMM.ModManager.CurrentProfile.Name}' was merged successfully.",
+                "Merge Successful!", NotificationType.Success, TimeSpan.FromDays(5));
         }
         catch (Exception ex) {
-            TKMM.Logger.LogError(ex, "An error occured when merging the selected profile '{Profile}'.", TKMM.ModManager.CurrentProfile.Name);
+            TKMM.Logger.LogError(ex, "An error occured when merging the selected profile '{Profile}'.",
+                TKMM.ModManager.CurrentProfile.Name);
             await ErrorDialog.ShowAsync(ex);
         }
     } 
