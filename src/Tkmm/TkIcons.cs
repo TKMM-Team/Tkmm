e6ceb5ef15e83ae146ad9bfd4482ecd8307078f1
ArchLeaders <archleadership28@gmail.com>
2024-10-06 22:14:10 -0700

    Update TkIcons

--- src/Tkmm/TkIcons.cs
+++ src/Tkmm/TkIcons.cs
@@ -2,7 +2,8 @@ namespace Tkmm;
 
 public static class TkIcons
 {
-    public const string READY = "fa-regular fa-message-check";
+    public const string CIRCLE_CHECK = "fa-solid fa-circle-check";
     public const string PROGRESS = "fa-solid fa-arrow-progress";
+    public const string READY = "fa-regular fa-message-check";
     public const string WARNING = "fa-solid fa-triangle-exclamation";
 }
