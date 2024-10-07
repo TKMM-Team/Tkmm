c2e72fac34c0a94de75de952ae6571b9caeaa16c
ArchLeaders <archleadership28@gmail.com>
2024-10-06 21:38:50 -0700

    Create MenuExtension (ConfigureMenu)

---
+++ src/Tkmm/Extensions/MenuExtension.cs
@@ -0,0 +1,12 @@
+using MenuFactory.Abstractions;
+using Tkmm.Models.MenuModels;
+
+namespace Tkmm.Extensions;
+
+public static class MenuExtension
+{
+    public static void ConfigureMenu(this IMenuFactory factory)
+    {
+        factory.AddMenuGroup<FileMenuModel>();
+    }
+}
