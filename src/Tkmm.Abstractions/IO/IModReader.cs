0880accf5f7472118b7d292af8294d05ebb36341
ArchLeaders <archleadership28@gmail.com>
2024-10-08 19:32:57 -0700

    Include optional stream input in ReadMod

--- src/Tkmm.Abstractions/IO/IModReader.cs
+++ src/Tkmm.Abstractions/IO/IModReader.cs
@@ -2,7 +2,11 @@ namespace Tkmm.Abstractions.IO;
 
 public interface IModReader
 {
-    ValueTask<ITkMod?> ReadMod<T>(T? input, Ulid predefinedModId = default, CancellationToken ct = default) where T : class;
+    ValueTask<ITkMod?> ReadMod<T>(T? input, Stream? stream = null, CancellationToken ct = default) where T : class
+        => ReadMod(input, stream, predefinedModId: default, ct);
+    
+    ValueTask<ITkMod?> ReadMod<T>(T? input, Stream? stream = null, Ulid predefinedModId = default,
+        CancellationToken ct = default) where T : class;
     
     bool IsKnownInput<T>(T? input) where T : class;
 }
