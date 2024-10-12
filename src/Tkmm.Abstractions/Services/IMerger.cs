9764ef76ed4962f5cd3d0b2b6f2e1549a2d14770
ArchLeaders <archleadership28@gmail.com>
2024-10-12 00:14:57 -0700

    Update IMerger.cs Include methods to merge with a vanilla buffer

--- src/Tkmm.Abstractions/Services/IMerger.cs
+++ src/Tkmm.Abstractions/Services/IMerger.cs
@@ -2,7 +2,11 @@ namespace Tkmm.Abstractions.Services;
 
 public interface IMerger
 {
-    ValueTask Merge(IEnumerable<ArraySegment<byte>> inputs, Func<Stream> getOutput, CancellationToken ct = default);
+    ValueTask Merge(IEnumerable<ArraySegment<byte>> inputs,
+        Func<ValueTask<Stream>> getOutput, CancellationToken ct = default);
+    
+    ValueTask Merge(IEnumerable<ArraySegment<byte>> inputs, ArraySegment<byte> vanilla,
+        Func<ValueTask<Stream>> getOutput, CancellationToken ct = default);
     
     bool IsKnownFile(in TkFileInfo fileInfo);
 }
