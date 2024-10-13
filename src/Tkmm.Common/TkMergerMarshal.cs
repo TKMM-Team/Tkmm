c5627f01bd34f18c5f67c8f3292feae10d717fea
ArchLeaders <archleadership28@gmail.com>
2024-10-12 20:24:26 -0700

    Create and implement TkMergerMarshal.cs

---
+++ src/Tkmm.Common/TkMergerMarshal.cs
@@ -0,0 +1,100 @@
+using Tkmm.Abstractions;
+using Tkmm.Abstractions.IO;
+using Tkmm.Abstractions.IO.Buffers;
+using Tkmm.Abstractions.Providers;
+using Tkmm.Abstractions.Services;
+using OrderedTarget = (string FileName, Tkmm.Abstractions.ChangelogEntry Entry, System.Collections.Generic.IEnumerable<Tkmm.Abstractions.ITkModChangelog> Mods);
+
+namespace Tkmm.Common;
+
+/// <summary>
+/// Simple marshal over the <see cref="IMergerProvider"/> to merge an <see cref="ITkProfile"/>.
+/// </summary>
+public sealed class TkMergerMarshal(IModManager manager, IMergerProvider mergerProvider, IRomfs romfs)
+{
+    private readonly IModManager _manager = manager;
+    private readonly IMergerProvider _mergerProvider = mergerProvider;
+    private readonly IRomfs _romfs = romfs;
+
+    /// <summary>
+    /// Merges the provided <see cref="ITkProfile"/> into the <paramref name="mergedOutputWriter"/> <see cref="IModWriter"/>.
+    /// </summary>
+    /// <param name="profile">The profile to merge.</param>
+    /// <param name="mergedOutputWriter">The output mod writer.</param>
+    /// <param name="ct"></param>
+    public async Task Merge(ITkProfile profile, IModWriter mergedOutputWriter, CancellationToken ct = default)
+    {
+        IEnumerable<OrderedTarget> targets = profile.Mods
+            .Where(profileMod => profileMod.IsEnabled)
+            .SelectMany(
+                profileMod => _manager
+                    .GetConfiguredOptions(profileMod.Mod)
+                    .Append(profileMod.Mod)
+            )
+            .Reverse()
+            .SelectMany(
+                changelog => changelog.Manifest
+                    .Select(kvp => (FileName: kvp.Key, Entry: kvp.Value, Mod: changelog))
+            )
+            .GroupBy(
+                tuple => (
+                    tuple.FileName,
+                    tuple.Entry
+                ),
+                tuple => tuple.Mod
+            )
+            .Select(
+                grouping => (
+                    grouping.Key.FileName,
+                    grouping.Key.Entry,
+                    grouping.AsEnumerable()
+                )
+            );
+
+        TkResourceSizeTable resourceSizeTable = new();
+
+        foreach ((string canonical, ChangelogEntry entry, IEnumerable<ITkModChangelog> mods) in targets) {
+            IMerger? merger = _mergerProvider.GetMerger(canonical);
+            if (merger is null) {
+                continue;
+            }
+
+            (Stream Stream, int Size)[] inputs = await Task.WhenAll(
+                mods.Select(
+                    mod => Task.Run(async () => await _manager.OpenModFile(mod, canonical, ct), ct)
+                )
+            );
+
+            using RentedBuffers<byte> buffers = await RentedBuffers<byte>.AllocateAsync(inputs, disposeStreams: true, ct);
+            using RentedBuffer<byte> vanilla = _romfs.GetVanilla(canonical, entry.Attributes);
+
+            if (!vanilla.Span.IsEmpty) {
+                await merger.Merge(buffers, vanilla.Segment,
+                    await OpenWriteOutput(canonical, entry.Attributes, mergedOutputWriter),
+                    resourceSizeTable, ct);
+                continue;
+            }
+
+            await merger.Merge(buffers,
+                await OpenWriteOutput(canonical, entry.Attributes, mergedOutputWriter),
+                resourceSizeTable, ct);
+        }
+
+        await using Stream restblOutputStream = await mergedOutputWriter.OpenWrite(
+            Path.Combine("System", "Resource", $"ResourceSizeTable.Product.{_romfs.Version}.rsizetable.zs"));
+        await resourceSizeTable.Write(restblOutputStream, ct);
+    }
+
+    private ValueTask<Stream> OpenWriteOutput(string canonical, TkFileAttributes attributes, IModWriter writer)
+    {
+        string fileName = _romfs.AddressTable.TryGetValue(canonical, out string? versionedFileName)
+            ? versionedFileName
+            : canonical;
+
+        if (attributes.HasFlag(TkFileAttributes.HasZsExtension)) {
+            fileName += ".zs";
+        }
+
+        return writer.OpenWrite(fileName);
+    }
+}
