14cf899b74551e4f887b8a53ce775676baccdebe
ArchLeaders <archleadership28@gmail.com>
2024-10-12 20:23:52 -0700

    Pass IResourceSizeTable to mergers

--- src/Tkmm.Abstractions/Services/IMerger.cs
+++ src/Tkmm.Abstractions/Services/IMerger.cs
@@ -4,9 +4,11 @@ namespace Tkmm.Abstractions.Services;
 
 public interface IMerger
 {
-    ValueTask Merge(RentedBuffers<byte> inputs, Stream output, CancellationToken ct = default);
+    ValueTask Merge(RentedBuffers<byte> inputs, Stream output,
+        ITkResourceSizeTable resourceSizeTable, CancellationToken ct = default);
     
-    ValueTask Merge(RentedBuffers<byte> inputs, ArraySegment<byte> vanillaData, Stream output, CancellationToken ct = default);
+    ValueTask Merge(RentedBuffers<byte> inputs, ArraySegment<byte> vanillaData, Stream output,
+        ITkResourceSizeTable resourceSizeTable, CancellationToken ct = default);
     
     bool IsKnownFile(in TkFileInfo fileInfo);
 }
