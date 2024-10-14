9ef73ffcb994211f1271ca875bb89b937893dd8a
ArchLeaders <archleadership28@gmail.com>
2024-10-14 13:21:13 -0700

    Remove legacy parser code from gb mod object

--- src/Tkmm.GameBanana.Core/GameBananaMod.cs
+++ src/Tkmm.GameBanana.Core/GameBananaMod.cs
@@ -1,15 +1,9 @@
-﻿using System.Runtime.InteropServices;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Text.Json.Serialization.Metadata;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Text.Json.Serialization;
 
 namespace Tkmm.GameBanana.Core;
 
-public sealed partial class GameBananaMod : ObservableObject
+public sealed class GameBananaMod
 {
-    private const int GB_TOTK_ID = 7617;
-    
     [JsonPropertyName("_idRow")]
     public ulong Id { get; set; }
 
@@ -45,74 +39,6 @@ public sealed partial class GameBananaMod : ObservableObject
 
     [JsonPropertyName("_aCredits")]
     public List<GameBananaCreditGroup> Credits { get; set; } = [];
-
-//     public static async Task<ITkMod> FromId(string id)
-//     {
-//         GameBananaMod gamebananaMod = await DownloadMetaData(id);
-//
-//         if (gamebananaMod.Game.Id != GB_TOTK_ID) {
-//             throw new InvalidDataException($"""
-//                 The mod '{gamebananaMod.Name}' is not for TotK
-//                 """);
-//         }
-//
-//         if (gamebananaMod.Files.FirstOrDefault(x => Path.GetExtension(x.Name) is ".tkcl" or ".zip" or ".rar" or ".7z") is not GameBananaFile file) {
-//             throw new InvalidDataException($"""
-//                 The mod '{gamebananaMod.Name}' has no valid archive (tkcl, zip, rar, 7z) file
-//                 """);
-//         }
-//
-//         return await gamebananaMod.FromFile(file);
-//     }
-//
-//     public async Task<Mod> FromFile(GameBananaFile file)
-//     {
-//         byte[] data = await DownloadOperations.DownloadAndVerify(file.DownloadUrl, Convert.FromHexString(file.Checksum));
-//         using MemoryStream ms = new(data);
-//         Mod mod = await Mod.FromStream(ms, file.Name, GetTkModId(file));
-//
-//         ObservableCollection<ModContributor> contributors = [];
-//         foreach (var group in Credits) {
-//             foreach (var author in group.Authors) {
-//                 contributors.Add(new() {
-//                     Name = author.Name,
-//                     Contributions = [author.Role]
-//                 });
-//             }
-//         }
-//
-//         string thumbnailUrl = $"{Media.Images[0].BaseUrl}/{Media.Images[0].File}";
-//         using Stream thumbnailHttpStream = await DownloadOperations.Client.GetStreamAsync(thumbnailUrl);
-//
-//         string thumbnailPath = Path.Combine(mod.SourceFolder, PackageBuilder.THUMBNAIL);
-//         using (FileStream thumbnailFileStream = File.Create(thumbnailPath)) {
-//             await thumbnailHttpStream.CopyToAsync(thumbnailFileStream);
-//         }
-//
-//         mod.Name = Name;
-//         mod.Author = Submitter.Name;
-//         mod.Description = new ReverseMarkdown.Converter(new ReverseMarkdown.Config {
-//             GithubFlavored = true,
-//             ListBulletChar = '*',
-//             UnknownTags = ReverseMarkdown.Config.UnknownTagsOption.Bypass
-//         }).Convert(Text);
-//         mod.ThumbnailUri = PackageBuilder.THUMBNAIL;
-//         mod.Contributors = contributors;
-//         mod.Version = Version;
-//
-//         PackageBuilder.CreateMetaData(mod, mod.SourceFolder);
-//
-//         return mod;
-//     }
-
-    private Ulid GetTkModId(GameBananaFile gameBananaFile)
-    {
-        Span<byte> guidRawBuffer = stackalloc byte[16];
-        MemoryMarshal.Write(guidRawBuffer, Id);
-        MemoryMarshal.Write(guidRawBuffer[8..], gameBananaFile.Id);
-
-        return MemoryMarshal.Read<Ulid>(guidRawBuffer);
-    }
 }
 
 [JsonSerializable(typeof(GameBananaMod))]
