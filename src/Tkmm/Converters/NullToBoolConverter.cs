4c1e6487a5ea5af3dda8760f33ab2dc384f9cb5c
MasterBubbles <guiguy046@hotmail.fr>
2024-10-17 22:02:54 +0200

    Make password textbox conditional

--- src/Tkmm/Converters/NullToBoolConverter.cs
+++ src/Tkmm/Converters/NullToBoolConverter.cs
@@ -1,18 +1,19 @@
+using Avalonia.Data.Converters;
 using System;
 using System.Globalization;
-using Avalonia.Data.Converters;
 
-namespace Tkmm.Converters;
-
-public class NullToBoolConverter : IValueConverter
+namespace Tkmm.Converters
 {
-    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    public class NullToBoolConverter : IValueConverter
     {
-        return value != null;
-    }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
 
-    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        throw new NotImplementedException();
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
