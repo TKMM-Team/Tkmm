c3c3841ee0805965d343743d66a4aefb78c1cd55
ArchLeaders <archleadership28@gmail.com>
2024-10-19 18:35:31 -0700

    Fix system string resource key

--- src/Clients/Tkmm.Core/Localization/StringResources_System.cs
+++ src/Clients/Tkmm.Core/Localization/StringResources_System.cs
@@ -4,7 +4,7 @@ public class StringResources_System
 {
     private const string GROUP = "System";
 
-    public string ToastNotice { get; } = GetStringResource(GROUP, nameof(ToastNotice));
+    public string Notice { get; } = GetStringResource(GROUP, nameof(Notice));
     public string DefaultProfileName { get; } = GetStringResource(GROUP, nameof(DefaultProfileName));
     public string UndefinedVersion { get; } = GetStringResource(GROUP, nameof(UndefinedVersion));
 }
