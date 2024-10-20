5ff69bbb4694062144c2e3b7df7ce07166d3a375
ArchLeaders <archleadership28@gmail.com>
2024-10-20 00:23:18 -0700

    Update tk json context

--- src/Tkmm.Models.Mvvm/TkJsonContext.cs
+++ src/Tkmm.Models.Mvvm/TkJsonContext.cs
@@ -5,4 +5,7 @@ namespace Tkmm.Models.Mvvm;
 [JsonSerializable(typeof(TkMod))]
 [JsonSerializable(typeof(TkModOption))]
 [JsonSerializable(typeof(TkModOptionGroup))]
+[JsonSerializable(typeof(TkProfile))]
+[JsonSerializable(typeof(TkModStorage))]
+[JsonSourceGenerationOptions(DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)]
 public sealed partial class TkJsonContext : JsonSerializerContext;
