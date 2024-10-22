10f1b05d023c3ca0fa2fac6a6571653815b4566e
ArchLeaders <archleadership28@gmail.com>
2024-10-21 18:49:07 -0700

    Make ModWriterProvider a singleton

--- src/Clients/Tkmm.Core/Providers/ModWriterProvider.cs
+++ src/Clients/Tkmm.Core/Providers/ModWriterProvider.cs
@@ -1,13 +1,14 @@
 using Tkmm.Abstractions;
 using Tkmm.Abstractions.IO;
 using Tkmm.Abstractions.Providers;
-using Tkmm.Core.IO;
 using Tkmm.Core.IO.ModWriters;
 
 namespace Tkmm.Core.Providers;
 
 public class ModWriterProvider : IModWriterProvider
 {
+    public static readonly ModWriterProvider Instance = new();
+    
     public IModWriter GetSystemWriter(ModContext context)
     {
         return new SystemModWriter(context.Id);
