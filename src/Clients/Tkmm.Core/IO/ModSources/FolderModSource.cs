f0e2a27d5e5f268ba476bd80215f9f96ec5c2e27
ArchLeaders <archleadership28@gmail.com>
2024-10-21 18:49:50 -0700

    Update folder mod source implementation

--- src/Clients/Tkmm.Core/IO/ModSources/FolderModSource.cs
+++ src/Clients/Tkmm.Core/IO/ModSources/FolderModSource.cs
@@ -1,27 +1,32 @@
+using System.Runtime.CompilerServices;
 using Tkmm.Abstractions.IO;
 
 namespace Tkmm.Core.IO.ModSources;
 
-public sealed class FolderModSource(string sourceFolder) : IModSource
+public sealed class FolderModSource(string sourceFolder) : ModSource<string>(Path.Combine(sourceFolder, "romfs"))
 {
-    public string RomfsPath { get; } = Path.Combine(sourceFolder, "romfs");
-
-    public IEnumerable<string> RomfsFiles
+    protected override IEnumerable<string> RomfsFiles
         => Directory.EnumerateFiles(RomfsPath, "*.*", SearchOption.AllDirectories);
 
-    public ValueTask<(Stream Stream, long StreamLength)> OpenRead(string file, CancellationToken ct = default)
+    protected override ValueTask<(Stream Stream, long StreamLength)> OpenRead(string input, CancellationToken ct = default)
     {
-        Stream fs = File.OpenRead(file);
+        Stream fs = File.OpenRead(input);
 
         return ValueTask.FromResult(
             (fs, fs.Length)
         );
     }
 
-    public ValueTask<bool> IsKnownInput<T>(T? input) where T : class
+    public override ValueTask<bool> IsKnownInput<TSource>(TSource? input) where TSource : class
     {
         return ValueTask.FromResult(
             input is string directory && Directory.Exists(directory)
         );
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    protected override string GetFileName(string input)
+    {
+        return input;
     }
 }
