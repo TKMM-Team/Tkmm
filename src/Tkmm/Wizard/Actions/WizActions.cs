a8662bcf33df0dfc5b6d294ce202abdbf511ea76
ArchLeaders <archleadership28@gmail.com>
2024-12-28 00:39:56 -0800

    Update wizard actions

--- src/Tkmm/Wizard/Actions/WizActions.cs
+++ src/Tkmm/Wizard/Actions/WizActions.cs
@@ -1,5 +1,7 @@
+using System.Text;
 using Avalonia.Platform.Storage;
 using LibHac.Common.Keys;
+using LibHac.Tools.Fs;
 using Tkmm.Core;
 using Tkmm.Dialogs;
 using Tkmm.Helpers;
@@ -13,7 +15,7 @@ public static class WizActions
             OperatingSystem.IsWindows() ? "*.exe" : "*"
         ]
     };
-    
+
     public static async ValueTask<(bool, int?)> SetupOtherEmulator()
     {
         string? emulatorFilePath = await App.XamlRoot.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions {
@@ -33,7 +35,7 @@ public static class WizActions
 
         return await StartEmulatorSetup(emulatorFilePath);
     }
-    
+
     public static async ValueTask<(bool, int?)> StartRyujinxSetup()
     {
         if (RyujinxHelper.GetRyujinxDataFolder(out string? ryujinxExeFilePath) is not string ryujinxDataFolder || ryujinxExeFilePath is null) {
@@ -74,35 +76,35 @@ public static class WizActions
         }
 
         Config.Shared.EmulatorPath = ryujinxExeFilePath;
-        
+
         TkConfig.Shared.KeysFolderPath = ryujinxKeysFolder;
         TkConfig.Shared.BaseGameFilePath = baseGameFilePath.FilePath;
         TkConfig.Shared.GameUpdateFilePath = tkFiles
             .OrderBy(x => x.Version)
             .Last()
             .FilePath;
-        
+
         return (true, null);
     }
-    
+
     public static async ValueTask<(bool, int?)> StartEmulatorSetup(string emulatorFilePath)
     {
         Config.Shared.EmulatorPath = emulatorFilePath;
-        
+
         string emulatorName = Path.GetFileNameWithoutExtension(emulatorFilePath);
         string emulatorDataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), emulatorName);
 
         if (!Directory.Exists(emulatorDataFolder)) {
             return (true, null);
         }
-        
+
         if (EmulatorHelper.GetKeys(emulatorDataFolder, out string emulatorKeysFolder) is not KeySet keys) {
             await MessageDialog.Show(
                 $"The required keys could not be found in your {emulatorName} installation.",
                 "Setup Error");
             return (false, null);
         }
-        
+
         TkConfig.Shared.KeysFolderPath = emulatorKeysFolder;
 
         (string FilePath, string Version)[] tkFiles = EmulatorHelper.GetTotkFiles(emulatorName, emulatorDataFolder, keys).ToArray();
@@ -118,7 +120,7 @@ public static class WizActions
             else {
                 TkConfig.Shared.GameUpdateFilePath = tkFiles[0].FilePath;
             }
-            
+
             return (true, null);
         }
 
@@ -126,18 +128,56 @@ public static class WizActions
             .OrderBy(x => x.Version)
             .Last()
             .FilePath;
-        
+
         if (tkFiles.FirstOrDefault(x => x.Version == "1.0.0") is not (FilePath: string, Version: string) baseGameFilePath) {
             return (true, null);
         }
-        
+
         TkConfig.Shared.BaseGameFilePath = baseGameFilePath.FilePath;
-        
+
         return (true, null);
     }
-    
+
     public static async ValueTask<(bool, int?)> VerifyConfig()
     {
-        throw new NotImplementedException();
+        StringBuilder reasons = new();
+
+        if (string.IsNullOrWhiteSpace(TkConfig.Shared.GameDumpFolderPath)) {
+            reasons.AppendLine("[RomFS Dump] The game dump folder path is empty.");
+            goto UseNspXci;
+        }
+
+        if (TkRomHelper.GetVersionFromRomfs(TkConfig.Shared.GameDumpFolderPath) is not int version) {
+            reasons.AppendLine("[RomFS Dump] Invalid game dump folder path.");
+            goto UseNspXci;
+        }
+
+        if (version == 100) {
+            reasons.AppendLine("[RomFS Dump] Invalid game dump version (1.0.0), TKMM requires version 1.1.0 or later.");
+        }
+
+    UseNspXci:
+        if (TkConfig.Shared.KeysFolderPath is not string keysFolderPath || TkRomHelper.GetKeys(keysFolderPath) is not KeySet keys) {
+            reasons.AppendLine("[XCI/NSP] The keys folder is invalid.");
+            goto Fail;
+        }
+
+        if (TkConfig.Shared.BaseGameFilePath is not string baseGameFilePath ||
+            !TkRomHelper.IsTotkRomFile(baseGameFilePath, keys, out Application? app) || app.DisplayVersion != "1.0.0") {
+            reasons.AppendLine("[XCI/NSP] The base game file is invalid.");
+            goto Fail;
+        }
+
+        if (TkConfig.Shared.GameUpdateFilePath is not string gameUpdateFilePath ||
+            !TkRomHelper.IsTotkRomFile(gameUpdateFilePath, keys, out Application? update) || update.DisplayVersion == "1.0.0") {
+            reasons.AppendLine("[XCI/NSP] The game update file is invalid.");
+            goto Fail;
+        }
+
+        return (true, null);
+
+    Fail:
+        await MessageDialog.Show(reasons, "Configuration Errors");
+        return (false, null);
     }
 }
