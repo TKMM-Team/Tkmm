7025dfc5e0868948b19bc52f06d39e62c7e859d7
MasterBubbles <guiguy046@hotmail.fr>
2025-01-30 20:43:57 +0100

    Remove 'Export to SD Card' from NX version

--- src/Tkmm/Models/MenuModels/ToolsMenuModel.cs
+++ src/Tkmm/Models/MenuModels/ToolsMenuModel.cs
@@ -7,9 +7,11 @@ namespace Tkmm.Models.MenuModels;
 
 public sealed class ToolsMenuModel
 {
+#if !TARGET_NX
     [TkMenu(TkLocale.Menu_ToolsExportToSdCard, TkLocale.Menu_Tools, InputGesture = "Ctrl + E", Icon = TkIcons.SD_CARD)]
     public static Task ExportToSdCard()
     {
         return MergeActions.Instance.ExportToSdCard();
     }
+#endif
 }
