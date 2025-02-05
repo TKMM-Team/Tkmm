ceef798fae25fbae8151f28ae23e80f97ba5e7cb
ArchLeaders <archleadership28@gmail.com>
2025-02-05 14:30:13 -0800

    Impl symlink helper

---
+++ src/Tkmm.Core/Helpers/SymlinkHelper.cs
@@ -0,0 +1,83 @@
+using System.Diagnostics;
+using System.Runtime.Versioning;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Microsoft.Win32;
+using TkSharp.Core;
+
+namespace Tkmm.Core.Helpers;
+
+public class SymlinkHelper
+{
+    public static async ValueTask CreateMany(IEnumerable<string> targets, string linkToPath)
+    {
+        if (OperatingSystem.IsWindows() && !IsDeveloperModeEnabled()) {
+            await CreateManyRequestPermission(targets, linkToPath);
+            return;
+        }
+
+        Create(targets, linkToPath);
+    }
+
+    private static void Create(IEnumerable<string> targets, string linkToPath)
+    {
+        foreach (string path in targets) {
+            if (CheckFolder(path)) {
+                Directory.CreateSymbolicLink(path, linkToPath);
+            }
+        }
+    }
+
+    [SupportedOSPlatform("windows")]
+    private static async ValueTask CreateManyRequestPermission(IEnumerable<string> targets, string linkToPath)
+    {
+        StringBuilder arguments = new();
+
+        foreach (string path in targets) {
+            if (CheckFolder(path)) {
+                arguments.Append($"""
+                    MKLINK /D "{path}" "{linkToPath}" &&
+                    """);
+            }
+        }
+
+        ProcessStartInfo processInfo = new() {
+            CreateNoWindow = true,
+            WindowStyle = ProcessWindowStyle.Hidden,
+            FileName = "cmd.exe",
+            Arguments = $"/c {arguments}ECHO Success",
+            UseShellExecute = true,
+            Verb = "runas"
+        };
+
+        if (Process.Start(processInfo) is { } process) {
+            await process.WaitForExitAsync();
+        }
+    }
+
+    [SupportedOSPlatform("windows")]
+    private static bool IsDeveloperModeEnabled()
+    {
+        RegistryKey? key = Registry.LocalMachine.OpenSubKey(
+            @"SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock", writable: false);
+        return key?.GetValue("AllowDevelopmentWithoutDevLicense", 0) as int? == 1;
+    }
+
+    private static bool CheckFolder(string path)
+    {
+        if (!Directory.Exists(path) && Path.GetDirectoryName(path) is string parent) {
+            Directory.CreateDirectory(parent);
+            return true;
+        }
+
+        if (Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories).Any()) {
+            TkLog.Instance.LogWarning(
+                "The export location '{ExportLocationPath}' cannot be created because it has contents.",
+                path);
+            return false;
+        }
+        
+        Directory.Delete(path, recursive: true);
+        return true;
+    }
+}
--- src/Tkmm.Core/Models/ExportLocations.cs
+++ src/Tkmm.Core/Models/ExportLocations.cs
@@ -1,5 +1,8 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Logging;
+using Tkmm.Core.Helpers;
+using TkSharp.Core;
 
 namespace Tkmm.Core.Models;
 
@@ -15,5 +18,26 @@ public sealed partial class ExportLocations : ObservableCollection<ExportLocatio
     private void Delete(ExportLocation target)
     {
         Remove(target);
+    }
+
+    public async ValueTask Create()
+    {
+        await SymlinkHelper.CreateMany(GetValidTargets(this), TKMM.MergedOutputFolder);
+    }
+
+    private static IEnumerable<string> GetValidTargets(IEnumerable<ExportLocation> locations)
+    {
+        foreach (ExportLocation location in locations.Where(x => x.IsEnabled)) {
+            string path = location.SymlinkPath;
+            if (Directory.Exists(path) && Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories).Any()) {
+                location.IsEnabled = false;
+                TkLog.Instance.LogWarning(
+                    "Export location '{ExportLocationPath}' was omitted and disabled because the folder contained contents.",
+                    path);
+                continue;
+            }
+            
+            yield return path;
+        }
     }
 }
