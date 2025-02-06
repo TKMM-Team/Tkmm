0af12fd8b61543f5630f6c2a96517aac160bb8be
ArchLeaders <archleadership28@gmail.com>
2025-02-05 19:08:20 -0800

    Misc. cleanup

--- src/Tkmm/Wizard/Pages/GameDumpConfigPageContext.cs
+++ src/Tkmm/Wizard/Pages/GameDumpConfigPageContext.cs
@@ -7,56 +7,16 @@ namespace Tkmm.Wizard.Pages;
 
 public sealed partial class GameDumpConfigPageContext : ObservableObject
 {
-    private static readonly FilePickerFileType _nsp = new("NSP") {
-        Patterns = [
-            "*.nsp"
-        ]
-    };
-
-    private static readonly FilePickerFileType _xci = new("XCI") {
-        Patterns = [
-            "*.xci"
-        ]
-    };
-
-    private static readonly FilePickerFileType _xciNsp = new("XCI/NSP") {
-        Patterns = [
-            "*.xci", "*.nsp"
-        ]
-    };
-
     [RelayCommand]
-    private static async Task Browse(TextBox target)
+    private static async Task Browse(TextBox tb)
     {
-        string? result = target.Tag switch {
-            "folder" => await BrowseFolder(),
-            _ => await BrowseFile(target.Tag as string)
-        };
-
-        target.Text = result;
-    }
-
-    private static async ValueTask<string?> BrowseFolder()
-    {
-        return await App.XamlRoot.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions {
+        string? result = await App.XamlRoot.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions {
             AllowMultiple = false
         }) switch {
             [IStorageFolder target] => target.TryGetLocalPath(),
             _ => null
         };
-    }
 
-    private static async ValueTask<string?> BrowseFile(string? types)
-    {
-        return await App.XamlRoot.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions {
-            AllowMultiple = false,
-            FileTypeFilter = types switch {
-                "xci,nsp" => [_xciNsp, _xci, _nsp],
-                _ => [_nsp]
-            }
-        }) switch {
-            [IStorageFile target] => target.TryGetLocalPath(),
-            _ => null
-        };
+        tb.Text = result;
     }
 }
--- src/Tkmm/Wizard/Pages/NxDumpConfigPage.axaml.cs
+++ src/Tkmm/Wizard/Pages/NxDumpConfigPage.axaml.cs
@@ -1,16 +1,11 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.VisualTree;
-using System.Threading.Tasks;
-using Tkmm.Core;
 
-namespace Tkmm.Wizard.Pages
+namespace Tkmm.Wizard.Pages;
+
+public partial class NxDumpConfigPage : UserControl
 {
-    public partial class NxDumpConfigPage : UserControl
+    public NxDumpConfigPage()
     {
-        public NxDumpConfigPage()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
-} 
+}
