01bef2738a7b60fd9254a856671d95a38c86cd29
MasterBubbles <guiguy046@hotmail.fr>
2025-02-07 18:20:52 +0100

    Fix relative embedded source path

--- src/Tkmm.Core/Services/TkOptimizerService.cs
+++ src/Tkmm.Core/Services/TkOptimizerService.cs
@@ -22,7 +22,7 @@ public class TkOptimizerService
             },
             Source = new EmbeddedSource(
                 "Tkmm.Resources.UltraCam",
-                Assembly.GetCallingAssembly())
+                Assembly.GetEntryAssembly())
         };
     }
 }
