030ffb79f4a827c2b1a3f068180e7ad2b1eef760
MasterBubbles <guiguy046@hotmail.fr>
2025-02-07 18:20:52 +0100

    Fix relative embedded source path

--- src/Tkmm.Core/IO/EmbeddedSource.cs
+++ src/Tkmm.Core/IO/EmbeddedSource.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Reflection;
 using TkSharp.Core;
 
@@ -7,11 +8,13 @@ public class EmbeddedSource(string root, Assembly assembly) : ITkSystemSource
 {
     public Stream OpenRead(string relativeFilePath)
     {
+        relativeFilePath = relativeFilePath.Replace('/', '.').Replace('\\', '.');
         return assembly.GetManifestResourceStream($"{root}.{relativeFilePath}")!;
     }
 
     public bool Exists(string relativeFilePath)
     {
+        relativeFilePath = relativeFilePath.Replace('/', '.').Replace('\\', '.');
         return assembly.GetManifestResourceInfo($"{root}.{relativeFilePath}") is not null;
     }
 }
