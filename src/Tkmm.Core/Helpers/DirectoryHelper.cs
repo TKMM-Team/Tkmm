095e80a6fe6ab399194b50abcbacdaa052cae870
ArchLeaders <archleadership28@gmail.com>
2025-02-07 17:20:48 -0800

    Impl recursive delete with filter

--- src/Tkmm.Core/Helpers/DirectoryHelper.cs
+++ src/Tkmm.Core/Helpers/DirectoryHelper.cs
@@ -21,6 +21,55 @@ public static class DirectoryHelper
             Directory.Delete(target, recursive);
         }
     }
+    
+    public static void DeleteTargetsFromDirectory(string targetDirectory, string[] targets, Func<string, bool> filter, bool recursive = false)
+    {
+        foreach (string target in targets) {
+            string absolutePath = Path.Combine(targetDirectory, target);
+            DeleteTarget(absolutePath, filter, recursive);
+        }
+    }
+    
+    private static void DeleteTarget(string target, Func<string, bool> filter, bool recursive = false)
+    {
+        if (File.Exists(target) && filter(target)) {
+            File.Delete(target);
+            return;
+        }
+        
+        if (Directory.Exists(target)) {
+            if (DeleteTargetFolder(target, filter, recursive)) {
+                Directory.Delete(target);
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Returns true if the <paramref name="target"/> can be deleted.
+    /// </summary>
+    private static bool DeleteTargetFolder(string target, Func<string, bool> filter, bool recursive = false)
+    {
+        int deleted = 0;
+        string[] files = Directory.GetFiles(target);
+        
+        foreach (string file in files.Where(filter)) {
+            deleted++;
+            File.Delete(file);
+        }
+        
+        string[] folders = Directory.GetDirectories(target);
+        
+        if (!recursive) {
+            return folders.Length + files.Length == deleted;
+        }
+
+        foreach (string folder in folders.Where(folder => filter(folder) && DeleteTargetFolder(folder, filter, recursive))) {
+            deleted++;
+            Directory.Delete(folder);
+        }
+        
+        return folders.Length + files.Length == deleted;
+    }
 
     public static void Copy(string source, string output, bool overwrite = false)
     {
