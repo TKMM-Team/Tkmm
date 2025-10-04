7f3da9d9a9ae5978ef6dceccba11cc76ad586679
LordBubbles <48891568+LordBubblesDev@users.noreply.github.com>
2025-10-04 20:17:47 +0200

    GB mod viewer: clean up image switching animations

--- src/Tkmm/Views/Pages/GameBananaModPageView.axaml.cs
+++ src/Tkmm/Views/Pages/GameBananaModPageView.axaml.cs
@@ -1,14 +1,18 @@
 using Avalonia.Controls;
+using Avalonia.Animation;
+using Avalonia.Animation.Easings;
+using Avalonia.Styling;
 using Avalonia.Media;
-using Avalonia.Threading;
 using Tkmm.ViewModels.Pages;
 
 namespace Tkmm.Views.Pages;
 
 public partial class GameBananaModPageView : UserControl
 {
-    private Border? _imageContainer;
+    private Image? _mainImage;
+    private TranslateTransform? _imageTransform;
     private int _previousImageIndex = -1;
+    private CancellationTokenSource? _animationCancellation;
 
     public GameBananaModPageView()
     {
@@ -18,45 +22,53 @@ public partial class GameBananaModPageView : UserControl
 
     private void OnDataContextChanged(object? sender, EventArgs e)
     {
-        if (DataContext is GameBananaModPageViewModel viewModel)
-        {
-            _imageContainer = this.FindControl<Border>("ImageContainer");
-            if (_imageContainer != null)
-            {
-                _imageContainer.RenderTransform = new TranslateTransform();
-            }
-            viewModel.PropertyChanged += OnViewModelPropertyChanged;
+        if (DataContext is not GameBananaModPageViewModel viewModel) {
+            return;
         }
+        
+        _mainImage = this.FindControl<Image>("MainImage");
+        
+        if (_mainImage?.RenderTransform is TranslateTransform transform) {
+            _imageTransform = transform;
+        }
+        
+        viewModel.PropertyChanged += OnViewModelPropertyChanged;
     }
 
     private void OnViewModelPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
     {
-        if (e.PropertyName == nameof(GameBananaModPageViewModel.SelectedImageIndex) && _imageContainer?.RenderTransform is TranslateTransform transform)
-        {
-            var viewModel = (GameBananaModPageViewModel)sender!;
-            
-            if (_previousImageIndex >= 0)
-            {
-                // Simple slide animation
-                var direction = viewModel.SelectedImageIndex > _previousImageIndex ? 100 : -100;
-                transform.X = direction;
-                
-                // Animate to center
-                _ = Task.Run(async () =>
-                {
-                    for (int i = 0; i < 20; i++)
-                    {
-                        await Task.Delay(15);
-                        var progress = i / 19.0;
-                        var eased = 1 - Math.Pow(1 - progress, 3); // Cubic ease out
-                        var x = direction * (1 - eased);
-                        
-                        Dispatcher.UIThread.Post(() => transform.X = x);
-                    }
-                });
-            }
-            
-            _previousImageIndex = viewModel.SelectedImageIndex;
+        if (e.PropertyName != nameof(GameBananaModPageViewModel.SelectedImageIndex) ||
+            _mainImage == null || _imageTransform == null) {
+            return;
         }
+        var viewModel = (GameBananaModPageViewModel)sender!;
+            
+        if (_previousImageIndex >= 0) {
+            _animationCancellation?.Cancel();
+            _animationCancellation?.Dispose();
+            _animationCancellation = new CancellationTokenSource();
+                
+            var direction = viewModel.SelectedImageIndex > _previousImageIndex ? 200 : -200;
+                
+            var animation = new Animation {
+                Duration = TimeSpan.FromMilliseconds(400),
+                Easing = new CubicEaseOut(),
+                Children =
+                {
+                    new KeyFrame {
+                        Cue = new Cue(0.0),
+                        Setters = { new Setter(TranslateTransform.XProperty, (double)direction) }
+                    },
+                    new KeyFrame {
+                        Cue = new Cue(1.0),
+                        Setters = { new Setter(TranslateTransform.XProperty, 0.0) }
+                    }
+                }
+            };
+                
+            _ = animation.RunAsync(_mainImage, _animationCancellation.Token);
+        }
+            
+        _previousImageIndex = viewModel.SelectedImageIndex;
     }
 }
