5c7fa0928f8f33489d53cf5c6e981816081fcf05
MediaMoots <81146974+MediaMoots@users.noreply.github.com>
2026-07-22 17:45:20 +0800

    A new advanced packaging option to preserve select authored values

---
+++ src/Tkmm/Dialogs/ResourceSizeOverrideImportView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Tkmm.Dialogs;
+
+public partial class ResourceSizeOverrideImportView : UserControl
+{
+    public ResourceSizeOverrideImportView()
+    {
+        InitializeComponent();
+    }
+}
