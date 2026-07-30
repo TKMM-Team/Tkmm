9c2be119cec00fb58fc1ee053f0cc61076d92d9c
LordBubbles <48891568+LordBubblesDev@users.noreply.github.com>
2026-07-30 11:07:05 +0200

    Adjust UI layout for RESTBL overrides

--- src/Tkmm/ViewModels/ResourceSizeOverrideEntryViewModel.cs
+++ src/Tkmm/ViewModels/ResourceSizeOverrideEntryViewModel.cs
@@ -4,7 +4,8 @@ namespace Tkmm.ViewModels;
 
 public sealed partial class ResourceSizeOverrideEntryViewModel(string canonical, uint size) : ObservableObject
 {
-    public string Canonical { get; } = canonical;
+    [ObservableProperty]
+    private string _canonical = canonical;
 
     [ObservableProperty]
     private uint _size = size;
